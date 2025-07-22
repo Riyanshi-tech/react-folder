81ce68da3499b5a727bc4229ce4ad71223329d20
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-07-22 18:01:49 +0530

    background change

--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -10,20 +10,24 @@ function App() {
   return (
     <div className="container"> 
       <UserCard 
-        name="crystal princess" 
-        desc="She is gorgeous and pure-hearted" 
-        img={fairyimg1}
-      /> 
-      <UserCard 
-        name="forest fairy" 
-        desc="She is nature-loving and kind" 
-        img={fairyimg2}
-      />
-      <UserCard 
-        name="golden blossom" 
-        desc="She is magical and spreads happiness" 
-        img={fairyimg3}
-      />
+  name="crystal princess" 
+  desc="She is gorgeous and pure-hearted" 
+  img={fairyimg1}
+  bgColor="#fdb5cdff"  // pink-ish
+/> 
+<UserCard 
+  name="forest fairy" 
+  desc="She is nature-loving and kind" 
+  img={fairyimg2}
+  bgColor="#a2fca9ff"  // green-ish
+/>
+<UserCard 
+  name="golden blossom" 
+  desc="She is magical and spreads happiness" 
+  img={fairyimg3}
+  bgColor="#f7d977ff"  // yellow-ish
+/>
+
     </div>
   );
 }
--- react-project-one/src/components/UserCard.jsx
+++ react-project-one/src/components/UserCard.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import "./UserCard.css";
 
-
-
 const UserCard = (props) => {
   return (
-    <div className='user-container'>
+    <div className='user-container' style={{ backgroundColor: props.bgColor }}>
         <p id='user-name'>{props.name}</p>
         <img id='user-image' src={props.img} alt={props.name} />
         <p id='user-description'>{props.desc}</p>
