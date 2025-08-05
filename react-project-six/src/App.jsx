e288105b05d6a4a0fe7cdf0a42e1cbf6223a727a
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-08-05 17:25:19 +0530

    project-six

--- react-project-six/src/App.jsx
+++ react-project-six/src/App.jsx
@@ -2,8 +2,25 @@
 import './App.css'
 
 function App() {
-  
-  return 
+  // function handleclick(){
+  //   alert("I am clicked")
+  // }
+  // function handlemouseover(){
+  //   alert("you are on the para")
+  // }
+  function handleinputchange(e){
+    // console.log("input value get changed")
+    console.log("value till now:", e.target.value);
+  }
+  return (
+    <div>
+      <form >
+        <input type="text" onchange={handleinputchange} />
+      </form>
+      {/* <p onMouseOver={handlemouseover} style={{border:"1px solid black"}}>I am Paragraph</p>
+    <button onClick={handleclick}>click me</button> */}
+    </div>
+  )
     
 }
 
