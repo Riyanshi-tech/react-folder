0595f894079734d426c16ae235ff51f2462861cb
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-08-10 22:15:31 +0530

    useeffect hook

--- react-project-seven/src/App.jsx
+++ react-project-seven/src/App.jsx
@@ -3,12 +3,42 @@ import './App.css'
 import {useEffect,useState} from 'react'
 function App() {
   const [count,setCount] = useState(0);
+  const [total,setTotal] = useState(1);
  useEffect(() => {
    alert("I will render on each click")
    
  })
+//  variation 2
+//   useEffect(() => {
+   
+//  alert("Iwill run on only 1st render")
+//    }
+//  }, [])
+// variation 3
+// useEffect(() => {
+//   alert("I will run every time when count is updated")
+//   }
+// }, [count])
+// variation  4 multiple dependencies 
+// useEffect(() => {
+//   
+//   }
+// }, [count,totald])
+// variation 5:
+// now using the clean up function
+// useEffect(() => {
+//   alert("count is updated ")
+
+//   return () => {
+//     alert("count is mounted from UI");
+
+//   }
+// }, [count])
+
+ 
  function handleClick(){
   setCount(count+1);
+  setTotal(total+1);
  }
  
 
@@ -17,6 +47,10 @@ function App() {
       <button onClick={handleClick}>click me</button>
       <br />
       count is:{count}
+      <br />
+      <button onClick={handleClick}>click me</button>
+      <br />
+      total is:{total}
     </div>
   )
 }
