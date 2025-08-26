d1eb56fbb3f6e78089259b7cd6c7144f9e40a617
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-08-26 10:18:25 +0530

    counter with constraints

--- react-project-seven/src/App.jsx
+++ react-project-seven/src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
+import { useState } from 'react'
 // import {useEffect,useState} from 'react'
-function App() {
+
 //   const [count,setCount] = useState(0);
 //   const [total,setTotal] = useState(1);
 //  useEffect(() => {
@@ -39,14 +40,47 @@ function App() {
 //  function handleClick(){
 //   setCount(count+1);
 //   setTotal(total+1);
-//  }
- 
+//  }import { useState } from 'react'
+
+
+function App() {
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    setCounter(prevCounter => {
+      if (prevCounter < 20) {
+        return prevCounter + 1
+      } else {
+        return prevCounter // do nothing if already 20
+      }
+    })
+  }
+
+  const removeValue = () => {
+    setCounter(prevCounter => {
+      if (prevCounter > 0) {
+        return prevCounter - 1
+      } else {
+        return prevCounter // do nothing if already 0
+      }
+    })
+  }
 
   return (
-    <div>
-      
-     </div>
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+      <p>footer: {counter}</p>
+    </>
   )
 }
 
 export default App
+
+ 
+
+  
