b25252011c16c576ab0ec1afea8ba1c1afcd8838
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-09-10 22:37:19 +0530

    react components

---
+++ react-begin/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>welcome to website</h1>
+        <nav>
+            <a href="">home</a>
+            <br />
+            <a href="">contact</a>
+            <br />
+            <a href="">about</a>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Header
