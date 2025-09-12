fe2cc971e2178c8dee4383c376d4395938e4561e
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-09-12 22:53:54 +0530

    inserting icons

--- react-begin/src/App.jsx
+++ react-begin/src/App.jsx
@@ -1,9 +1,11 @@
 
 import React from 'react'
 import Header from './components/Header'
+import { Gi3dGlasses } from "react-icons/gi";
 const App = () => {
   return (
-    <Header/>
+    
+    <Gi3dGlasses />
   )
 }
 
--- react-begin/src/components/Header.jsx
+++ react-begin/src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
+      
         <h1>welcome to website</h1>
         <nav>
             <a href="">home</a>
